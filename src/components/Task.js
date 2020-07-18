4ca67c85972ed0cf8b5d6e96151751082a1d25d2
patrick <patrick204nqh@gmail.com>
2020-07-18 10:48:59 +0700

    display task follow TASKS_STATUSES

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
+const TASKS_STATUSES = ['Unstarted', 'In Progress', 'Completed'];
+
 const Task = (props) => {
   return (
-    <div>
-      <h2>{props.task.title}</h2>
-      <p>{props.task.description}</p>
-    </div>
+    <>
+      <form>
+        <select defaultValue={props.task.status}>
+          {TASKS_STATUSES.map((status) => (
+            <option value={status} key={status} />
+          ))}
+        </select>
+      </form>
+      <h2
+        className='card-title mt-3 text-uppercase px-2'
+        style={{ color: '#3a4' }}
+      >
+        {props.task.title}
+      </h2>
+      <p className='card-text mb-3 text-muted font-weight-bold px-2'>
+        {props.task.description}
+      </p>
+    </>
   );
 };
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,13 +3,19 @@ const initialState = [
     id: 1,
     title: 'Learn ReactJS',
     description: "Let's learn React JS today",
-    status: 'In Progress',
+    status: 'Completed',
   },
   {
     id: 2,
     title: 'Learn Redux',
     description: "Let's learn Redux today",
     status: 'In Progress',
+  },
+  {
+    id: 3,
+    title: 'Learn MERN',
+    description: "Let's learn MERN this is my next step!",
+    status: 'Unstarted',
   },
 ];
 
